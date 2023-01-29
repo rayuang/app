7e4f2a4267ed51917d6a4845e067420634c61292
rayuang <rayuang@163.com>
2023-01-30 00:43:49 +0800

    my_wx_mini_program_dev

---
+++ my_wx_mini_programs/pages/notes/notes.js
@@ -0,0 +1,86 @@
+// pages/notes/notes.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    notes: [{
+        title: "java基础知识1",
+        content: "abcdefghiabcdefghiabcdefghiabcdefghiabcdefghiabcdefghiabcdefghiabcdefghiabcdefghiabcdefghi"
+      },
+      {
+        title: "java基础知识2",
+        content: "abcdefghiabcdefghiabcdefghiabcdefghiabcdefghi"
+      },
+      {
+        title: "java基础知识3",
+        content: "abcdefghiabcdefghiabcdefghiabcdefghiabcdefghi"
+      },
+      {
+        title: "java基础知识4",
+        content: "abcdefghiabcdefghiabcdefghiabcdefghiabcdefghi"
+      },
+      {
+        title: "java基础知识5",
+        content: "abcdefghiabcdefghiabcdefghiabcdefghiabcdefghi"
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
